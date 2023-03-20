ef3c815f6daa2b9b882677bd4922d333215a264f
AIMtoCod <109942506+AIMtoCod@users.noreply.github.com>
2023-03-20 18:50:44 +0100

    Update libft.h

--- libft.h
+++ libft.h
@@ -14,16 +14,16 @@
 # define LIBFT_H
 # include <unistd.h>
 
-int		isalpha(int c);
-int		isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
+int		  isalpha(int c);
+int		  isdigit(int c);
+int		  ft_isalnum(int c);
+int		  ft_isascii(int c);
+int		  ft_isprint(int c);
 size_t	ft_strlen(const char *s);
-void	*ft_memset(void *s, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
+void	  *ft_memset(void *s, int c, size_t n);
+void	  ft_bzero(void *s, size_t n);
+void	  *ft_memcpy(void *dst, const void *src, size_t n);
+void	  *ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
