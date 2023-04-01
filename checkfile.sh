428e55d2cb49e76a26ef75baa54c6554718a8a2f
Harsh Kumbhani <109942506+harshkumbhani@users.noreply.github.com>
2023-04-02 00:18:26 +0200

    Edited with color code

--- checkfile.sh
+++ checkfile.sh
@@ -35,15 +35,15 @@ ft_putchar_fd.c \
 ft_putstr_fd.c \
 ft_putendl_fd.c \
 ft_putnbr_fd.c \
-ft_lstnew.c \
-ft_lstadd_front.c \
-ft_lstsize.c \
-ft_lstlast.c \
-ft_lstadd_back.c \
-ft_lstdelone.c \
-ft_lstclear.c \
-ft_lstiter.c \
-ft_lstmap.c"
+ft_lstnew_bonus.c \
+ft_lstadd_front_bonus.c \
+ft_lstsize_bonus.c \
+ft_lstlast_bonus.c \
+ft_lstadd_back_bonus.c \
+ft_lstdelone_bonus.c \
+ft_lstclear_bonus.c \
+ft_lstiter_bonus.c \
+ft_lstmap_bonus.c"
 
 MISSING_FILES=""
 
@@ -55,13 +55,14 @@ if [ -d "$DIR" ]; then
         fi
     done
     if [ "$MISSING_FILES" != "" ]; then
+		printf "\033[31mMissing files found in $DIR\033[0m\n"
         echo "The following files were not found in $DIR:"
         for file in $MISSING_FILES; do
             echo "- $file"
         done
         exit 1
     else
-        echo "All files found in $DIR"
+        printf "\033[32mAll files found in $DIR\033[0m"
         exit 0
     fi
 else
