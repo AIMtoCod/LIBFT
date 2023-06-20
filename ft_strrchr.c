aed74af108c5efe7d82a6cdf1ec7425bee405b9a
Harsh Kumbhani <109942506+harshkumbhani@users.noreply.github.com>
2023-06-20 10:08:05 +0200

    Edited strrchr function

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,21 +6,36 @@
 /*   By: hkumbhan <hkumbhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 14:51:34 by hkumbhan          #+#    #+#             */
-/*   Updated: 2023/04/02 23:29:50 by hkumbhan         ###   ########.fr       */
+/*   Updated: 2023/04/05 10:27:42 by hkumbhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/// @brief Locates first occurence of (char c) in string c
+/// @brief Locates last occurence of (char c) in string c
 /// @param s The string to be searched for 
 /// @param c The character c
 /// @return Pointer to the located character or NULL if c doesnot appear in s
 char	*ft_strrchr(const char *s, int c)
 {
+	int	i;
+
+	i = ft_strlen(s);
+	while (i >= 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+		i--;
+	}
+	return (NULL);
+}
+
+/* Algorithm 2
+	char	*ft_strrchr(const char *s, int c)
+{
 	char	*ptr;
 
-	ptr = 0;
+	ptr = NULL;
 	if (*s == (char) c)
 		ptr = (char *)s;
 	while (*s++)
@@ -30,8 +45,4 @@ char	*ft_strrchr(const char *s, int c)
 	}
 	return (ptr);
 }
-
-//int main()
-//{
-//	printf("%s\n", ft_strrchr("Harsh i Smart", 'a'));
-//}
+*/
