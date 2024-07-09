cfeaeae1e0773544857ef4b4d359191e5e3b6f09
Harsh <Harshwut@icloud.com>
2024-07-09 18:55:20 +0530

    add: ft_isspace function definition

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: harsh <harsh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 19:36:18 by hkumbhan          #+#    #+#             */
-/*   Updated: 2024/07/09 18:48:28 by harsh            ###   ########.fr       */
+/*   Updated: 2024/07/09 18:54:57 by harsh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+int		ft_isspace(int c);
 
 
 // String search and comparison functions
